ccade9fe5dd402b22ab5267faa27b7eb5ba551b1
Brendan T White <brendan@thespia.com>
2021-05-11 22:04:26 +0930

    Basic grid of cards now looking smooth and sweet

--- resources/views/potato.blade.php
+++ resources/views/potato.blade.php
@@ -17,8 +17,7 @@
 
 <!-- Container -->
 <div
-  class="mt-10 max-w-screen-xl mx-auto px-4 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
-  bg-red-200 sm:bg-yellow-200 md:bg-green-200 lg:bg-blue-200"
+  class="mt-10 max-w-screen-xl mx-auto px-6 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
 >
   
   
@@ -27,53 +26,98 @@
   <div class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg">
     <div class="flex-1">
       <h2 class="text-gray-900 text-2xl font-bold leading-snug">
-        Tailwind v1.1.0
+        One
       </h2>
       <p class="mt-2 text-lg">
-        Tailwind v1.1.0 has been released with some cool new features and a
-        couple of bug fixes. This is the first feature release since the v1.0
-        release, so let's dig into some of the updates.
+        One.
       </p>
     </div>
     <a
       href="#"
       class="mt-6 inline-block px-6 py-3 text-center text-white font-semibold bg-blue-700 rounded-md shadow-sm"
     >
-      View article
+      One
     </a>
   </div>
+
+
   <!-- Grid column -->
   <div class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg">
     <div class="flex-1">
       <h2 class="text-gray-900 text-2xl font-bold leading-snug">
-        Getting Started with Tailwind CSS Custom Forms
-      </h2>
-      <p class="mt-2 text-lg">Shorter content for accentuation</p>
-    </div>
-    <a
-      href="#"
-      class="mt-6 inline-block px-6 py-3 text-center text-white font-semibold bg-blue-700 rounded-md shadow-sm"
-    >
-      View article
-    </a>
-  </div>
-  <!-- Grid column -->
-  <div class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg">
-    <div class="flex-1">
-      <h2 class="text-gray-900 text-2xl font-bold leading-snug">
-        11 Benefits of Tailwind CSS
+        Two
       </h2>
       <p class="mt-2 text-lg">
-        I've been using Tailwind CSS professionally almost every day for nearly
-        two years. Here I share some of the benefits I've gained by using
-        Tailwind.
+        Two.
       </p>
     </div>
     <a
       href="#"
       class="mt-6 inline-block px-6 py-3 text-center text-white font-semibold bg-blue-700 rounded-md shadow-sm"
     >
-      View article
+      Two
+    </a>
+  </div>
+
+
+  <!-- Grid column -->
+  <div class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg">
+    <div class="flex-1">
+      <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+        Three
+      </h2>
+      <p class="mt-2 text-lg">
+        Three.
+      </p>
+    </div>
+    <a
+      href="#"
+      class="mt-6 inline-block px-6 py-3 text-center text-white font-semibold bg-blue-700 rounded-md shadow-sm"
+    >
+      Three
+    </a>
+  </div>
+  
+  
+  
+
+  <!-- Grid column -->
+  <div class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg">
+    <div class="flex-1">
+      <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+        Four
+      </h2>
+      <p class="mt-2 text-lg">
+        Four.
+      </p>
+    </div>
+    <a
+      href="#"
+      class="mt-6 inline-block px-6 py-3 text-center text-white font-semibold bg-blue-700 rounded-md shadow-sm"
+    >
+      Four
+    </a>
+  </div>
+  
+  
+  
+  
+
+  <!-- Grid column -->
+  <div class="flex flex-col flex-1 px-10 py-12 bg-white rounded-lg shadow-lg">
+    <div class="flex-1">
+      <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+        Five
+      </h2>
+      <p class="mt-2 text-lg">
+        Five.
+      </p>
+    </div>
+    <a
+      href="#"
+      class="mt-6 inline-block px-6 py-3 text-center text-white font-semibold bg-blue-700 rounded-md shadow-sm"
+    >
+      Five
     </a>
   </div>
   
