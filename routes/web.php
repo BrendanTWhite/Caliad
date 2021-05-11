d2d42eb9389273496a938e0be0e206a37e000413
Brendan T White <brendan@thespia.com>
2021-05-11 18:59:09 +0930

    Added potato

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/potato', function () {
+    return view('potato');
+})->middleware(['auth'])->name('potato');
+
 require __DIR__.'/auth.php';
