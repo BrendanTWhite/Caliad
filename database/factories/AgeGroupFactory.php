4f07553666578b720fc1d1c25949ac6b0f224734
Brendan T White <brendan@thespia.com>
2021-05-14 13:24:22 +0930

    Age Groups boilerplate

---
+++ database/factories/AgeGroupFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AgeGroup;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AgeGroupFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AgeGroup::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
