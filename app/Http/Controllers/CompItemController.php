8215248887246a3af71051fd11ccb7f1a8edcfd3
Brendan T White <brendan@thespia.com>
2021-05-14 19:23:30 +0930

    CompItem boilerplate

---
+++ app/Http/Controllers/CompItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CompItem;
+use Illuminate\Http\Request;
+
+class CompItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CompItem  $compItem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CompItem $compItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CompItem  $compItem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CompItem $compItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CompItem  $compItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CompItem $compItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CompItem  $compItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CompItem $compItem)
+    {
+        //
+    }
+}
