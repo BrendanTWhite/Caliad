eba065391654c5ae6534a250c16596a3ee41d1d3
Brendan T White <brendan@thespia.com>
2021-05-14 19:28:45 +0930

    db changes for Routines

--- database/migrations/2021_05_14_095523_create_routines_table.php
+++ database/migrations/2021_05_14_095523_create_routines_table.php
@@ -15,6 +15,15 @@ class CreateRoutinesTable extends Migration
     {
         Schema::create('routines', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('team_id')->constrained();
+            $table->foreignId('comp_item_id')->constrained();
+
+            $table->integer('sequence');
+            $table->string('music_title');
+            $table->dateTime('start');
+            $table->dateTime('end');
+
             $table->timestamps();
         });
     }
