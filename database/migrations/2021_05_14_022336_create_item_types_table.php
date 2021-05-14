a12b0dde15ede7823f4be6aeb0207de76f1ef86e
Brendan T White <brendan@thespia.com>
2021-05-14 12:12:57 +0930

    Item Types database structure and data

--- database/migrations/2021_05_14_022336_create_item_types_table.php
+++ database/migrations/2021_05_14_022336_create_item_types_table.php
@@ -15,8 +15,23 @@ class CreateItemTypesTable extends Migration
     {
         Schema::create('item_types', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->integer('sequence');
+
             $table->timestamps();
         });
+
+
+        // This is a reference table with data that
+        // rarely if ever changes
+
+        DB::table('item_types')->insert([
+            [ 'sequence' => 100, 'name' => 'Core' ],
+            [ 'sequence' => 200, 'name' => 'Fancy' ],
+            [ 'sequence' => 300, 'name' => 'Solos / Duos' ],
+        ]);
+
     }
 
     /**
