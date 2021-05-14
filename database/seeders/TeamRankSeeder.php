5ce5c722063da2d9b76e5093650b379af5224d30
Brendan T White <brendan@thespia.com>
2021-05-14 16:49:36 +0930

    team rank boilerplate

---
+++ database/seeders/TeamRankSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TeamRankSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
