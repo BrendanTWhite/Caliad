bacfd1cd73c87ec19a3c4b08a240809dfcecd5bf
Brendan T White <brendan@thespia.com>
2021-05-15 00:27:01 +0930

    Data for a couple more

--- database/migrations/2021_05_14_091631_create_session_division_table.php
+++ database/migrations/2021_05_14_091631_create_session_division_table.php
@@ -21,6 +21,43 @@ class CreateSessionDivisionTable extends Migration
 
             $table->timestamps();
         });
+
+        // This is NOT a reference table 
+        // however I'm going to insert the data for the 
+        // May 2021 comps anyway.
+
+      DB::table('session_division')->insert([
+            [ 'session_id' =>   1, 'division_id' => 104 ],
+            [ 'session_id' =>   1, 'division_id' => 105 ],
+            [ 'session_id' =>   4, 'division_id' => 104 ],
+            [ 'session_id' =>   4, 'division_id' => 105 ],
+            [ 'session_id' =>  10, 'division_id' => 102 ],
+            [ 'session_id' =>  13, 'division_id' => 102 ],
+            [ 'session_id' =>  16, 'division_id' => 103 ],
+            [ 'session_id' =>  19, 'division_id' => 102 ],
+            [ 'session_id' =>  22, 'division_id' => 101 ],
+            [ 'session_id' =>  25, 'division_id' => 102 ],
+            [ 'session_id' =>  28, 'division_id' => 103 ],
+            [ 'session_id' =>  31, 'division_id' => 103 ],
+            [ 'session_id' =>  34, 'division_id' => 102 ],
+            [ 'session_id' =>  40, 'division_id' => 101 ],
+            [ 'session_id' =>  43, 'division_id' => 101 ],
+            [ 'session_id' =>  46, 'division_id' => 100 ],
+            [ 'session_id' =>  52, 'division_id' => 100 ],
+            [ 'session_id' =>  55, 'division_id' => 100 ],
+            [ 'session_id' =>  58, 'division_id' => 100 ],
+            [ 'session_id' =>  64, 'division_id' => 100 ],
+            [ 'session_id' =>  67, 'division_id' => 100 ],
+            [ 'session_id' =>  70, 'division_id' => 100 ],
+            [ 'session_id' =>  76, 'division_id' => 100 ],
+            [ 'session_id' =>  79, 'division_id' => 100 ],
+            [ 'session_id' =>  98, 'division_id' => 103 ],
+            [ 'session_id' =>  98, 'division_id' => 104 ],
+            [ 'session_id' => 119, 'division_id' => 101 ],
+            [ 'session_id' => 192, 'division_id' => 101 ],
+            [ 'session_id' => 223, 'division_id' => 101 ],
+        ]);
+
     }
 
     /**
--- database/migrations/2021_05_14_091827_create_session_team_rank_table.php
+++ database/migrations/2021_05_14_091827_create_session_team_rank_table.php
@@ -21,6 +21,53 @@ class CreateSessionTeamRankTable extends Migration
 
             $table->timestamps();
         });
+
+        // This is NOT a reference table 
+        // however I'm going to insert the data for the 
+        // May 2021 comps anyway.
+
+      DB::table('session_team_rank')->insert([
+            [ 'session_id' =>   1, 'team_rank_id' => 1 ],
+            [ 'session_id' =>   4, 'team_rank_id' => 1 ],
+            [ 'session_id' =>   4, 'team_rank_id' => 2 ],
+            [ 'session_id' =>   4, 'team_rank_id' => 3 ],
+            [ 'session_id' =>  10, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  13, 'team_rank_id' => 2 ],
+            [ 'session_id' =>  13, 'team_rank_id' => 3 ],
+            [ 'session_id' =>  16, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  19, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  19, 'team_rank_id' => 2 ],
+            [ 'session_id' =>  22, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  25, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  28, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  28, 'team_rank_id' => 2 ],
+            [ 'session_id' =>  31, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  34, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  34, 'team_rank_id' => 2 ],
+            [ 'session_id' =>  40, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  43, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  43, 'team_rank_id' => 2 ],
+            [ 'session_id' =>  46, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  52, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  55, 'team_rank_id' => 2 ],
+            [ 'session_id' =>  58, 'team_rank_id' => 3 ],
+            [ 'session_id' =>  58, 'team_rank_id' => 4 ],
+            [ 'session_id' =>  64, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  67, 'team_rank_id' => 2 ],
+            [ 'session_id' =>  67, 'team_rank_id' => 3 ],
+            [ 'session_id' =>  70, 'team_rank_id' => 3 ],
+            [ 'session_id' =>  70, 'team_rank_id' => 4 ],
+            [ 'session_id' =>  76, 'team_rank_id' => 2 ],
+            [ 'session_id' =>  79, 'team_rank_id' => 1 ],
+            [ 'session_id' =>  79, 'team_rank_id' => 2 ],
+            [ 'session_id' =>  98, 'team_rank_id' => 1 ],
+            [ 'session_id' => 119, 'team_rank_id' => 1 ],
+            [ 'session_id' => 119, 'team_rank_id' => 2 ],
+            [ 'session_id' => 192, 'team_rank_id' => 2 ],
+            [ 'session_id' => 192, 'team_rank_id' => 3 ],
+            [ 'session_id' => 223, 'team_rank_id' => 2 ],
+        ]);
+
     }
 
     /**
