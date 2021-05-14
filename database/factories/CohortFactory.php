18e926a9dfb9d52347a5e4e2c53809ea69c827e4
Brendan T White <brendan@thespia.com>
2021-05-14 18:30:16 +0930

    Cohort boilerplate

---
+++ database/factories/CohortFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Cohort;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CohortFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Cohort::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
