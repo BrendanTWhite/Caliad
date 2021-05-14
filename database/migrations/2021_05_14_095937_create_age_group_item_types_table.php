c15397b8af402f0971b1e1747595758665375143
Brendan T White <brendan@thespia.com>
2021-05-14 20:26:19 +0930

    age_group_item_types database structure and data

--- database/migrations/2021_05_14_095937_create_age_group_item_types_table.php
+++ database/migrations/2021_05_14_095937_create_age_group_item_types_table.php
@@ -15,8 +15,97 @@ class CreateAgeGroupItemTypesTable extends Migration
     {
         Schema::create('age_group_item_types', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('age_group_id')->constrained();
+            $table->foreignId('item_type_id')->constrained();
+
+            $table->integer('min_duration_seconds');
+            $table->integer('max_duration_seconds');
+
             $table->timestamps();
         });
+
+        // This is a reference table with data that
+        // rarely if ever changes
+
+        DB::table('age_group_item_types')->insert([
+
+            [
+                'age_group_id'         => 1, 
+                'item_type_id'         => 1, 
+                'min_duration_seconds' => 90,  
+                'max_duration_seconds' => 150,  
+            ], [ 
+                'age_group_id'         => 2, 
+                'item_type_id'         => 1, 
+                'min_duration_seconds' => 90,  
+                'max_duration_seconds' => 150,  
+            ], [ 
+                'age_group_id'         => 3, 
+                'item_type_id'         => 1, 
+                'min_duration_seconds' => 90,  
+                'max_duration_seconds' => 150,  
+            ], [ 
+                'age_group_id'         => 4, 
+                'item_type_id'         => 1, 
+                'min_duration_seconds' => 120,  
+                'max_duration_seconds' => 180,  
+            ], [ 
+                'age_group_id'         => 5, 
+                'item_type_id'         => 1, 
+                'min_duration_seconds' => 150,  
+                'max_duration_seconds' => 210,  
+            ], [ 
+                'age_group_id'         => 6, 
+                'item_type_id'         => 1, 
+                'min_duration_seconds' => 150,  
+                'max_duration_seconds' => 210,  
+            ], [ 
+                'age_group_id'         => 7, 
+                'item_type_id'         => 1, 
+                'min_duration_seconds' => 150,  
+                'max_duration_seconds' => 210,  
+            ], 
+
+            [ 
+                'age_group_id'         => 1, 
+                'item_type_id'         => 2, 
+                'min_duration_seconds' => 120,  
+                'max_duration_seconds' => 180,  
+            ], [ 
+                'age_group_id'         => 2, 
+                'item_type_id'         => 2, 
+                'min_duration_seconds' => 120,  
+                'max_duration_seconds' => 180,  
+            ], [ 
+                'age_group_id'         => 3, 
+                'item_type_id'         => 2, 
+                'min_duration_seconds' => 120,  
+                'max_duration_seconds' => 180,  
+            ], [ 
+                'age_group_id'         => 4, 
+                'item_type_id'         => 2, 
+                'min_duration_seconds' => 150,  
+                'max_duration_seconds' => 210,  
+            ], [ 
+                'age_group_id'         => 5, 
+                'item_type_id'         => 2, 
+                'min_duration_seconds' => 150,  
+                'max_duration_seconds' => 210,  
+            ], [ 
+                'age_group_id'         => 6, 
+                'item_type_id'         => 2, 
+                'min_duration_seconds' => 150,  
+                'max_duration_seconds' => 210,  
+            ], [ 
+                'age_group_id'         => 7, 
+                'item_type_id'         => 2, 
+                'min_duration_seconds' => 180,  
+                'max_duration_seconds' => 240,  
+            ],
+
+        ]);
+
     }
 
     /**
