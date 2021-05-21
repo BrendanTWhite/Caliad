6949439ba4460c967808986491b0d0126e22420e
Brendan T White <brendan@thespia.com>
2021-05-21 22:24:57 +0930

    now with IDs on TRs and links going to the TR IDs nicely - and looks just fine on my phone - but still doesn't actually save (or remove) the time.

--- resources/views/sessions/show.blade.php
+++ resources/views/sessions/show.blade.php
@@ -36,16 +36,16 @@
 
   @foreach ($section->routines as $routine)
 
-                <tr>
+                <tr id="routine-{{ $routine->id }}">
 
                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-90 text-left">
                      @isset($routine->start)
-                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                    <a href="#routine-{{ $routine->id }}"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
                       <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
                     </svg></a>
                     <span class="px-2">{{ $routine->start->isoFormat('h:mm') }}</span>
                     @else
-                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                    <a href="#routine-{{ $routine->id }}"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
                       <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
                     </svg></a>
                     @endisset
@@ -64,11 +64,11 @@
                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 text-right">
                     @isset($routine->end)
                     <span class="px-2">{{ $routine->end->isoFormat('h:mm') }}</span>
-                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                    <a href="#routine-{{ $routine->id }}"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
                       <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
                     </svg></a>
                     @else
-                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                    <a href="#routine-{{ $routine->id }}"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
                       <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
                     </svg></a>
                     @endisset
