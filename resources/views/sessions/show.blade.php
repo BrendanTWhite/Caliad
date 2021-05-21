b7ead72114efbe02cbc7985c570a693ca5d48653
Brendan T White <brendan@thespia.com>
2021-05-21 21:59:52 +0930

    looking fairly nice, but buttons don't work yet, and design doesn't work on phone size screens

--- resources/views/sessions/show.blade.php
+++ resources/views/sessions/show.blade.php
@@ -14,14 +14,14 @@
 
   <!-- Header -->
   <div class="mt-10 max-w-screen-xl mx-auto px-10">
-    <h2 class="text-gray-900 text-2xl font-bold leading-snug"> 
-      {{ $section->description }} 
+    <h2 class="text-gray-900 text-2xl font-bold leading-snug">
+      {{ $section->description }}
     </h2>
   </div>
 
 
 
- 
+
 <!-- Table from https://tailwindui.com/components/application-ui/lists/tables -->
 <!-- This example requires Tailwind CSS v2.0+ -->
   <div class="mt-10 max-w-screen-xl mx-auto px-10">
@@ -38,44 +38,46 @@
 
                 <tr>
 
-                  <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Start</a>
-                  </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    @isset($routine->start)
-                    {{ $routine->start->isoFormat('h:mm') }}                  
+                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-90 text-left">
+                     @isset($routine->start)
+                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                    </svg></a>
+                    <span class="px-2">{{ $routine->start->isoFormat('h:mm') }}</span>
+                    @else
+                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+                    </svg></a>
                     @endisset
                   </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Clear</a>
+
+                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                    <span class="px-1">{{ $routine->sequence }}</span>
+                    <span class="px-1">{{ $routine->team->cohort->club->short_name }}</span>
+                    <span class="px-1">Team {{ $routine->team->team_rank->id }}</span>
                   </td>
 
                   <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                    {{ $routine->sequence }}                  
-                    {{ $routine->team->cohort->club->short_name }}                  
-                    Team {{ $routine->team->team_rank->id }}                            
+                    {{ $routine->music_title }}
                   </td>
 
-                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                    {{ $routine->music_title }}                  
-                  </td>
-
-                  <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Clear</a>
-                  </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 text-right">
                     @isset($routine->end)
-                    {{ $routine->end->isoFormat('h:mm') }}                  
+                    <span class="px-2">{{ $routine->end->isoFormat('h:mm') }}</span>
+                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                    </svg></a>
+                    @else
+                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+                    </svg></a>
                     @endisset
-                  </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                    <a href="#" class="text-indigo-600 hover:text-indigo-900">End</a>
                   </td>
 
                 </tr>
 
   @endforeach
-                
+
               </tbody>
             </table>
           </div>
@@ -86,7 +88,7 @@
 
 
 
-  
+
 
 
 @endforeach
