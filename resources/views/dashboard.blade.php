be0e477a26eabf13fab9a3820f44214ed04f3b20
Brendan T White <brendan@thespia.com>
2021-05-23 22:17:52 +0930

    better look and feel

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,13 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    Welcome! 
+                </div>
+
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    This is a work in progress; please let Brendan know if 
+                    there's anything useful you think we should add.
                 </div>
             </div>
         </div>
